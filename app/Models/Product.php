380652c473cf82b555abfb43b4d9835accf1e4af
QuangHieu0711 <nguyenquanghieugl2019@gmail.com>
2024-12-23 00:51:29 +0700

    Update Model Product

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -9,4 +9,8 @@ class Product extends Model
     public $timestamps = false;
 
     protected $fillable = ['name', 'description', 'price', 'quantity'];
+    public function orders()
+    {
+        return $this->belongsToMany(Order::class, 'order_details'); // Mối quan hệ nhiều-nhiều 
+    }
 }
