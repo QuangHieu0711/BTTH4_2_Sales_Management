dc663b3171c792f9f3b31235dfe07336d0b63c97
QuangHieu0711 <nguyenquanghieugl2019@gmail.com>
2024-12-23 00:55:36 +0700

    Update route Order và OrderDetails

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,8 @@
 
 use App\Http\Controllers\ProductController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\OrderController;
+use App\Http\Controllers\OrderDetailController;
 
 Route::get("/", [ProductController::class,"home"])->name('home');
 Route::prefix('products')->group(function () {
@@ -11,4 +13,20 @@ Route::prefix('products')->group(function () {
     Route::get('/{id}/edit', [ProductController::class, 'edit'])->name('products.edit');
     Route::put('/{id}', [ProductController::class, 'update'])->name('products.update');
     Route::delete('/{id}', [ProductController::class, 'destroy'])->name('products.destroy');
+});
+Route::prefix('orders')->group(function () {
+    Route::get('/', [OrderController::class, 'index'])->name('orders.index'); 
+    Route::get('/create', [OrderController::class, 'create'])->name('orders.create'); 
+    Route::post('/store', [OrderController::class, 'store'])->name('orders.store');
+    Route::get('/{id}/edit', [OrderController::class, 'edit'])->name('orders.edit'); 
+    Route::put('/{id}', [OrderController::class, 'update'])->name('orders.update'); 
+    Route::delete('/{id}', [OrderController::class, 'destroy'])->name('orders.destroy'); 
+});
+Route::prefix('order_details')->group(function () {
+    Route::get('/', [OrderDetailController::class, 'index'])->name('orderdetails.index'); 
+    Route::get('/create', [OrderDetailController::class, 'create'])->name('orderdetails.create'); 
+    Route::post('/store', [OrderDetailController::class, 'store'])->name('orderdetails.store'); 
+    Route::get('/{id}/edit', [OrderDetailController::class, 'edit'])->name('orderdetails.edit'); 
+    Route::put('/{id}', [OrderDetailController::class, 'update'])->name('orderdetails.update'); 
+    Route::delete('/{id}', [OrderDetailController::class, 'destroy'])->name('orderdetails.destroy'); 
 });
