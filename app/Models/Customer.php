4e782f11d9d32a7c12175ee99410a7d0b4d72348
ThuyLienLuong <luonglien332@gmail.com>
2024-12-23 17:33:19 +0700

    update Model_Customer

--- app/Models/Customer.php
+++ app/Models/Customer.php
@@ -3,10 +3,16 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
 class Customer extends Model
 {
-    //
+    use HasFactory;
+
+    // Tắt tính năng timestamps
+    public $timestamps = false;
+
     // Khai báo tên bảng tương ứng trong cơ sở dữ liệu
     protected $table = 'customers';
 
@@ -16,8 +22,11 @@ class Customer extends Model
         'address',
         'phone',
         'email',
+        
     ];
-    public function products()
+
+    // Mối quan hệ nhiều-nhiều với bảng products
+    public function products(): BelongsToMany
     {
         return $this->belongsToMany(Product::class, 'customer_product', 'customer_id', 'product_id');
     }
