62ee70631a863097b04041e4bc93cd3c163a401d
QuangHieu0711 <nguyenquanghieugl2019@gmail.com>
2024-12-23 11:27:32 +0700

    Update Seeder

--- database/seeders/OrderSeeder.php
+++ database/seeders/OrderSeeder.php
@@ -16,13 +16,14 @@ class OrderSeeder extends Seeder
     
      public function run(): void
      {
-         $faker = Faker::create();
-         foreach (range(1,200) as $index) {
-             DB::table("orders")->insert([
-                 'customer_id' => $faker->numberBetween(1, 200),
-                 'order_date' => $faker->date('Y-m-d'),
-                 'status' => $faker->randomElement(['Pending', 'Completed', 'Cancelled', 'Processing']),
-             ]);
-         }
+        $faker = Faker::create();
+        $customerCount = DB::table('customers')->count();
+        foreach (range(1, 200) as $index) {
+            DB::table('orders')->insert([
+                'customer_id' => $faker->numberBetween(1, $customerCount),
+                'order_date' => $faker->date('Y-m-d'),
+                'status' => $faker->randomElement(['pending', 'completed', 'cancelled', 'processing']),
+            ]);
+        }
      }
 }
